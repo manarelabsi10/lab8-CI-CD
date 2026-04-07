49933932ba47821bbc446cd592711872ab36cd4f
manarelabsi10 <manarelabsi395@gmail.com>
2026-04-07 18:47:30 +0200

    Fix init.sql by adding CREATE TABLE

--- init.sql
+++ init.sql
@@ -1,9 +1,8 @@
 CREATE TABLE IF NOT EXISTS tasks (
-  id     SERIAL PRIMARY KEY,
-  name   TEXT NOT NULL,
-  status TEXT NOT NULL DEFAULT 'pending'
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  status VARCHAR(50) NOT NULL
 );
-
 INSERT INTO tasks (name, status) VALUES
   ('Milk',         'done'),
   ('Eggs',         'done'),
@@ -12,7 +11,3 @@ INSERT INTO tasks (name, status) VALUES
   ('Orange juice', 'pending');
   ('Tea',          'pending');
   ('Tea',          'pending');
-  ('Tea',          'pending');
-  ('Milk',         'done'),
-  ('Eggs',         'done'),
-  ('Bread',        'pending'),
